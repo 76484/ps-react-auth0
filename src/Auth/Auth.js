3865c8195e4a32176cc74b0d89c1b2c586563f5b
Steven Smith <steven.76484@gmail.com>
2020-04-03 23:00:02 -0400

    Adding handleAuthentication and setSession methods to Auth class

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -12,7 +12,29 @@ export default class Auth {
     });
   }
 
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push("/");
+      } else if (err) {
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for further details.`);
+        console.log(err);
+      }
+    });
+  };
+
   login = () => {
     this.auth0.authorize();
+  };
+
+  setSession = (authResult) => {
+    // set the time that the access token will expire
+    const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + Date.now());
+
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
   };
 }
