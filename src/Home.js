0730a5ff86b83cc390bcdefe20dad1a237cefd17
Steven Smith <steven.76484@gmail.com>
2020-04-03 23:07:27 -0400

    Adding Profile link for authenticated users on Home component

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home({ auth }) {
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={auth.login}>Log In</button>
+      {auth.isAuthenticated() ? (
+        <Link to="/profile">Profile</Link>
+      ) : (
+        <button onClick={auth.login}>Log In</button>
+      )}
     </div>
   );
 }
