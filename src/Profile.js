6f36fa73f35f151e55c24c13bb38fdddfff18d8b
Steven Smith <steven.76484@gmail.com>
2020-04-04 09:25:45 -0400

    Displaying profile data on profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Profile(props) {
-  return <h1>Profile</h1>;
+function Profile({ auth }) {
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    auth.getProfile((err, userProfile) => {
+      if (err) {
+        throw err;
+      }
+
+      setProfile(userProfile);
+    });
+  }, [auth]);
+
+  return profile ? (
+    <>
+      <h1>Profile</h1>
+      <p>{profile.nickname}</p>
+      <img
+        alt="profile pic"
+        style={{ maxHeight: 50, maxWidth: 50 }}
+        src={profile.picture}
+      />
+      <pre>{JSON.stringify(profile)}</pre>
+    </>
+  ) : null;
 }
 
 export default Profile;
