0f41c0374737410d218110f92f7ee4570d86b992
Steven Smith <steven.76484@gmail.com>
2020-04-04 22:01:58 -0400

    Adding private endpoint to API

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
 require("dotenv").config();
 
 const app = express();
 
+const checkJwt = jwt({
+  algorithms: ["RS256"],
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+});
+
 app.get("/public", function (req, res) {
   res.json({
     message: "Hello from a public API!",
+  });
+});
+
+app.get("/private", checkJwt, function (req, res) {
+  res.json({
+    message: "Hello from a private API!",
   });
 });
 
