7ac25e24092885931187f96853240108394bac10
Steven Smith <steven.76484@gmail.com>
2020-04-04 23:55:48 -0400

    Fixing call to removeItem from localStorage

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -64,7 +64,7 @@ export default class Auth {
     localStorage.removeItem("access_token");
     localStorage.removeItem("id_token");
     localStorage.removeItem("expires_at");
-    localStorage.setItem("scopes");
+    localStorage.removeItem("scopes");
 
     this.userProfile = null;
 
